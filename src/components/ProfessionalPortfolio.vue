e59f7e440a4f36723e47bad7785fc6cbab46395d
Jessica Robbins <jessicarobbins03@gmail.com>
2025-11-04 02:35:36 +0100

    more lil edits

--- src/components/ProfessionalPortfolio.vue
+++ src/components/ProfessionalPortfolio.vue
@@ -32,13 +32,15 @@ const doneProjectComponents = [
   <button @click="$router.push('/')" class="back-button">
       <img src="../assets/BackButton.svg" alt="Back Button" class="back-icon" width="20px" height="20px" color="#ebeff1"/>
   </button>
-  <MainPage />
-  <CurrentProjects />
-  <div v-for="(Component, index) in curProjectComponents" :key="index" class="project-tab">
-    <component :is="Component" />
-  </div>
-  <CompletedProjects />
-  <div v-for="(Component, index) in doneProjectComponents" :key="index" class="project-tab">
-    <component :is="Component" />
+  <div style="margin: 0 10%">
+    <MainPage />
+    <CurrentProjects />
+    <div v-for="(Component, index) in curProjectComponents" :key="index" class="project-tab">
+      <component :is="Component" />
+    </div>
+    <CompletedProjects />
+    <div v-for="(Component, index) in doneProjectComponents" :key="index" class="project-tab">
+      <component :is="Component" />
+    </div>
   </div>
 </template>
